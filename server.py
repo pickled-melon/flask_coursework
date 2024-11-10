7b847cfb87f6e72096a9a85746c3f84fee1f06f5
pickled-melon <andy.typing@mail.ru>
2024-11-10 22:32:53 +0800

    Добавил глоссарий

--- server.py
+++ server.py
@@ -24,4 +24,16 @@ def about_page():
 
 @app.route("/glossary")
 def glossary_page():
-    return render_template("glossary.html", name="Глоссарий")
+    glossary = [
+        {'term': 'Lorem', 'defenition': 'Далеко-далеко за словесными горами, в стране гласных и согласных живут рыбные тексты.'},
+        {'term': 'Labore', 'defenition': 'Проектах это парадигматическая, выйти lorem его встретил то!'},
+        {'term': 'Iste', 'defenition': ' Текста вдали языкового ведущими переулка, океана страна живет правилами жаренные пустился букв осталось рекламных грустный послушавшись ее на берегу которое прямо что!'},
+        {'term': 'Doloribus', 'defenition': 'Прямо бросил на берегу, напоивший подпоясал, правилами букв обеспечивает заглавных грустный строчка журчит, страну запятых текста снова?'},
+        {'term': 'Aut', 'defenition': 'О безопасную пунктуация, жаренные сих предупредила рыбного!'},
+    ]
+
+
+    # Сортируем глоссарий по алфавиту
+    glossary = sorted(glossary, key=lambda d: d["term"])
+    
+    return render_template("glossary.html", name="Глоссарий", glossary=glossary)
